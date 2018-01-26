66b4a26135f9270e172dc0181b190cce54223d14
QIAN SU <2274593s@student.gla.ac.uk>
2018-01-26 20:23:08 +0000

    chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    # To display individual fields
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
